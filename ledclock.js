2f920646f9fd32349e92e2d89b630af4373a6128
KC <chocokrizx@gmail.com>
2014-12-09 01:25:29 -0700

    Implemented the following things:
    localStorage for alarms
    assigned selector to variable
    moved setInterval to avoid race condition
    organized javascript code

--- ledclock.js
+++ ledclock.js
@@ -16,39 +16,16 @@ function setDay(dow) {
 }
 
 function setAM(){
-    $('#AMPM').html("AM");
+    ampm.html('AM');
     return 'yay';
 }
 
 function setPM(){
-    $('#AMPM').html("PM");
+    ampm.html('PM');
     return 'woo';
 }
 
-$ ( function (){
-    updateTime();
-    $(".chosen-select").chosen({max_selected_options: 5});
-
-    $(".stopAlarm").click(function() {
-        console.log('clicked!');
-        $('#alarmSound').get(0).pause();
-        $('#alarmicon').removeClass('fa-bell-o').addClass('fa-bell-slash-o');
-        return false;
-    });
-
-    $('.saveAlarm').click(function(){
-        console.log('clicked!');
-        if (userAlarmTime.length > 4) {
-            alert('sorry, too many cooks! (alarms)');
-            return false;
-        }
-        userAlarmTime.push(getUserAlarm());
-        $('#alarmicon').removeClass('fa-bell-slash-o').addClass('fa-bell-o');
-        renderAlarms();
-        return false;
-    });
-});
-
+var ampm = $('#AMPM');
 var userAlarmTime = [];
 
 function renderAlarms() {
@@ -65,8 +42,13 @@ function renderAlarms() {
     $('.deleteAlarm').click(function() {
         console.log("clicked~");
         delete_alarm();
+        persistAlarms();
         return false;
     });
+}
+
+function persistAlarms() {
+    localStorage.setItem('objectAlarm', JSON.stringify(userAlarmTime));
 }
 
 function delete_alarm(index){
@@ -82,20 +64,18 @@ function updateTime() {
     }
     toggleMeridiem(now[7]);
 
-        for (i=0; i < userAlarmTime.length; i++) {
-            if (now === userAlarmTime[i].momentTime) {
-                $('#alarmSound').get(0).play();
-                alert("BEEP BEEP BEEP!")
-            }
+    for (i=0; i < userAlarmTime.length; i++) {
+        if (now === userAlarmTime[i].momentTime) {
+            alarmTheUser();
         }
-
+    }
     setDay(moment().day());
 }
 
-
-window.setInterval(function (){
-    updateTime();
-}, 1000);
+function alarmTheUser() {
+    $('#alarmSound').get(0).play();
+    alert("BEEP BEEP BEEP!");
+}
 
 function toggleMeridiem(a) {
     if (a === "a"){
@@ -109,9 +89,9 @@ function getUserAlarm() {
     var userHour = $('#alarmHour').val();
     var userMinute = $('#alarmMinute').val();
     var userMeridiem = $('#alarmMeridiem').val();
-
-    var alarmTime = userHour + userMinute + "00" + " " + userMeridiem;
     var peopleTime = userHour + ":" + userMinute + userMeridiem;
+    var alarmTime = userHour + userMinute + "00" + " " + userMeridiem;
+
     if (userHour === "" || userMinute === "" || userMeridiem === "") {
         alert('Please put in a completed alarm time, thanks! :D ');
         removeClass('a-bell-slash-o');
@@ -125,4 +105,38 @@ function getUserAlarm() {
     return objectTime;
 }
 
-// if now is equal to alarm time, pop an alert box
+$ ( function (){
+    if (localStorage.getItem('objectAlarm') == null) {
+        userAlarmTime = [];
+    } else {
+        userAlarmTime = JSON.parse(localStorage.getItem('objectAlarm'));
+    };
+    renderAlarms();
+
+    updateTime();
+    $(".chosen-select").chosen({max_selected_options: 5});
+
+    $(".stopAlarm").click(function() {
+        console.log('clicked!');
+        $('#alarmSound').get(0).pause();
+        $('#alarmicon').removeClass('fa-bell-o').addClass('fa-bell-slash-o');
+        return false;
+    });
+
+    window.setInterval(function (){
+        updateTime();
+    }, 1000);
+
+    $('.saveAlarm').click(function(){
+        console.log('clicked!');
+        if (userAlarmTime.length > 4) {
+            alert('sorry, too many cooks! (alarms)');
+            return false;
+        }
+        userAlarmTime.push(getUserAlarm());
+        persistAlarms();
+        $('#alarmicon').removeClass('fa-bell-slash-o').addClass('fa-bell-o');
+        renderAlarms();
+        return false;
+    });
+});
